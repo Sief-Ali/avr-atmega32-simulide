694852d49394a4970b35470f60d291fd534a0284
Sief Ali <sief4136@gmail.com>
2026-05-13 23:02:27 +0300

    Added calc as lib example

---
+++ libs/includes/calc.h
@@ -0,0 +1,10 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdint.h>
+
+#define num_t int8_t
+
+num_t calc_add(num_t num1, num_t num2);
+
+#endif
---
+++ libs/src/calc.c
@@ -0,0 +1,6 @@
+#include "calc.h"
+
+num_t calc_add(int8_t num1, int8_t num2) {
+  num_t result = num1 + num2;
+  return result;
+}
